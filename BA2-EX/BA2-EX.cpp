647181c63a5daca386842e295b3e4d974a314bf6
Cing Chen <40751071+Cing-Chen@users.noreply.github.com>
2021-12-22 20:26:20 +0800

    BA2-EX done

--- BA2-EX/BA2-EX.cpp
+++ BA2-EX/BA2-EX.cpp
@@ -5,79 +5,70 @@
 using namespace std;
 
 struct Rule {
-    // if has a rule like 「A -> BCD」
-    // its nonTerminal would be A
-    // its production would be BCD
     char nonTerminal;
     string production;
+    bool hasLambda;
 public:
-    Rule(char nonTerminal, string production) {
+    Rule(char nonTerminal, string production, bool hasLambda) {
         this->nonTerminal = nonTerminal;
         this->production = production;
+        this->hasLambda = hasLambda;
     }
 };
 
-struct FirstSet {
-    // if A's first set contains b, c, d three symbol
-    // its nonTerminal would be A
-    // its set would be bcd
+struct FSet {
     char nonTerminal;
     string set;
 public:
-    FirstSet(char nonTerminal, string set) {
+    FSet(char nonTerminal, string set) {
         this->nonTerminal = nonTerminal;
         this->set = set;
     }
 };
 
-struct FollowSet {
-    char nonTerminal;
-    string set;
-public:
-    FollowSet(char nonTerminal, string set) {
-        this->nonTerminal = nonTerminal;
-        this->set = set;
-    }
-};
-
-int getFollowSetIndex(vector<FollowSet> *, char);
-int getFirstSetIndex(vector<FirstSet> *, char);
+int getRuleIndex(vector<Rule> *, char);
+int getFSetIndex(vector<FSet> *, char);
 
 int main() {
+    /* Process input */
     const int maxLineLength = 500;
 
-    // the code to handle input
     vector<Rule> rules;
     char input[maxLineLength];
 
-    // handle grammar part
     cin.getline(input, maxLineLength);
 
-    while(strcmp(input, "END_OF_GRAMMAR")) { // if same, strcmp would return 0
+    while(strcmp(input, "END_OF_GRAMMAR")) {    // If input is same as string, strcmp will return 0
         char nonTerminal = input[0];
         string production;
+        bool hasLambda = false;
+
+        for(int i = 2; i < strlen(input); i++) {    // If input contain ';', set hasLambda as true
+            if(input[i] == ';') {
+                hasLambda = true;
+            }
+        }
 
         for(int i = 2; i < strlen(input); i++) {
             if(input[i] != '|') {
                 production += input[i];
             }
-            else {
-                rules.push_back(Rule(nonTerminal, production));
+            else {  // If input contain '|' means there are more than one line of grammar
+                rules.push_back(Rule(nonTerminal, production, hasLambda));
                 production = "";
             }
         }
 
-        rules.push_back(Rule(nonTerminal, production));
+        rules.push_back(Rule(nonTerminal, production, hasLambda));
+
         cin.getline(input, maxLineLength);
     }
 
-    vector<FirstSet> firstSets;
-    vector<FollowSet> followSets;
+    vector<FSet> firstSets, followSets, lastFollowSet;
 
-    // handle first set part
     cin.getline(input, maxLineLength);
 
-    while(strcmp(input, "END_OF_FIRST_SET")) { // if same, strcmp would return 0
+    while(strcmp(input, "END_OF_FIRST_SET")) { // If input is same as string, strcmp will return 0
         char nonTerminal = input[0];
         string production;
 
@@ -85,59 +76,150 @@ int main() {
             production += input[i];
         }
 
-        firstSets.push_back(FirstSet(nonTerminal, production));
-        followSets.push_back(FollowSet(nonTerminal, ""));
+        firstSets.push_back(FSet(nonTerminal, production));
+        followSets.push_back(FSet(nonTerminal, ""));
+        lastFollowSet.push_back(FSet(nonTerminal, ""));
+
         cin.getline(input, maxLineLength);
     }
 
-    // sample code for observing the data, which constructed by input
-    // for(int i = 0; i < rules.size(); i++) {
-    //     cout << rules[i].nonTerminal << " -> " << rules[i].production << endl;
-    // }
+    /* Process lambda */
+    vector<Rule> lastRules = rules;
 
-    // for(int i = 0; i < firstSets.size(); i++) {
-    //     cout << firstSets[i].nonTerminal << "'s first set is " << firstSets[i].set << endl;
-    // }
+    while(true) {
+        bool breakKey = false;
 
-    vector<FollowSet> lastFollowSet;
+        for(int i = 0; i < rules.size(); i++) {
+            if(rules.at(i).hasLambda == true) {
+                continue;
+            }
 
-    for(int i = 0; i < followSets.size(); i++) {
-        followSets.at(i).set += "$";
+            for(int j = 0; j < rules.at(i).production.length(); j++) {
+                int ruleIndex_j = getRuleIndex(&rules, rules.at(i).production.at(j));
+
+                if(ruleIndex_j == -1) { // If rules.at(i).production.at(j) is terminal, ruleIndex_j = -1
+                    break;
+                }
+
+                if(rules.at(ruleIndex_j).hasLambda == false) {
+                    break;
+                }
+                else if(j == rules.at(i).production.length() - 1 && rules.at(ruleIndex_j).hasLambda == true) {
+                    rules.at(i).hasLambda = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < rules.size(); i++) {
+            // If rules is not same as lastRules, do the same thing until they are same as each other
+            if(rules.at(i).hasLambda != lastRules.at(i).hasLambda) {
+                break;
+            }
+            else if(i == rules.size() - 1 && rules.at(i).hasLambda == lastRules.at(i).hasLambda) {
+                breakKey = true;
+            }
+        }
+
+        if(breakKey == true) {
+            break;
+        }
+        else {
+            lastRules = rules;
+        }
     }
+
+    /* Do follow set */
+    followSets.at(0).set += "$";    // rule 1
 
     bool breakKey = false;
     while(true) {
-        for(int i = 0; i < rules.size(); i++) {
-            for(int j = 0; j < rules.at(i).production.length(); j++) {
-                int followSetIndex_j = getFollowSetIndex(&followSets, rules.at(i).production.at(j));
-                bool hasLambda = false;
+        for(int i = 0; i < rules.size(); i++) { // All grammar
+            for(int j = 0; j < rules.at(i).production.length(); j++) {  // All right hand side
+                bool allLambda = false;
+
+                int followSetIndex_j = getFSetIndex(&followSets, rules.at(i).production.at(j));
+
+                if(followSetIndex_j == -1) {    // If rules.at(i).production.at(j) is terminal
+                    continue;
+                }
 
                 for(int k = j + 1; k < rules.at(i).production.length(); k++) {
-                    hasLambda = false;
-                    int firstSetIndex_k = getFirstSetIndex(&firstSets, rules.at(i).production.at(k));
+                    int firstSetIndex_k = getFSetIndex(&firstSets, rules.at(i).production.at(k));
 
-                    for(int l = 0; l < firstSets.at(firstSetIndex_k).set.length(); l++) {
-                        if(firstSets.at(firstSetIndex_k).set.at(l) != ';') {
-                            followSets.at(followSetIndex_j).set += firstSets.at(firstSetIndex_k).set.at(l);
+                    if(firstSetIndex_k == -1) { // If rules.at(i).production.at(k) is terminal
+                        followSets.at(followSetIndex_j).set += rules.at(i).production.at(k);
+                        break;
+                    }
+                    else {
+                        /* rule 2 */
+                        followSets.at(followSetIndex_j).set += firstSets.at(firstSetIndex_k).set;
+
+                        int ruleIndex_k = getRuleIndex(&rules, rules.at(i).production.at(k));
+
+                        if(rules.at(ruleIndex_k).hasLambda != true) {
+                            break;
+                        }
+                        else if(k == rules.at(i).production.length() - 1 && rules.at(ruleIndex_k).hasLambda == true) {
+                            allLambda = true;
+                        }
+                        else if(rules.at(ruleIndex_k).hasLambda == true){
+                            continue;
                         }
                         else {
-                            hasLambda = true;
+                            cout << "This is impossible case." << endl;
                         }
-                    }
-
-                    if(!hasLambda) {
-                        break;
                     }
                 }
 
-                if(hasLambda) {
-                    int followSetIndex_i = getFollowSetIndex(&followSets, rules.at(i).nonTerminal);
+                if(j + 1 >= rules.at(i).production.length()) {  // To the end
+                    allLambda = true;
+                }
 
+                if(allLambda == true) {
+                    int followSetIndex_i = getFSetIndex(&followSets, rules.at(i).nonTerminal);
+
+                    /* rule 3 */
                     followSets.at(followSetIndex_j).set += followSets.at(followSetIndex_i).set;
                 }
             }
         }
 
+        for(int i = 0; i < followSets.size(); i++) {
+            /* Do bubble sort to sort the set from small to big of ascii code */
+            for(int j = 0; j < followSets.at(i).set.length(); j++) {
+                for(int k = 0; k < followSets.at(i).set.length() - 1; k++) {
+                    if(followSets.at(i).set.at(k) > followSets.at(i).set.at(k + 1)) {
+                        char temp = followSets.at(i).set.at(k + 1);
+                        followSets.at(i).set.at(k + 1) = followSets.at(i).set.at(k);
+                        followSets.at(i).set.at(k) = temp;
+                    }
+                }
+            }
+
+            while(true) {
+                bool done = true;
+                for(int j = 0; j < followSets.at(i).set.length() - 1; j++) {
+                    /* Delete repeat elements */
+                    if(followSets.at(i).set.at(j) == followSets.at(i).set.at(j + 1)) {
+                        followSets.at(i).set.erase(followSets.at(i).set.begin() + j);
+                        done = false;
+                        break;
+                    }
+                    /* Delete ';' */
+                    else if(followSets.at(i).set.at(j) == ';') {
+                        followSets.at(i).set.erase(followSets.at(i).set.begin() + j);
+                        done = false;
+                        break;
+                    }
+                }
+
+                if(done == true) {
+                    break;
+                }
+            }
+        }
+
+        /* Do the same thing until followSets is same as lastFollowSet */
         for(int i = 0; i < followSets.size(); i++) {
             if(followSets.at(i).set != lastFollowSet.at(i).set) {
                 for(int j = 0; j < followSets.size(); j++) {
@@ -149,31 +231,49 @@ int main() {
                 breakKey = true;
             }
         }
+
+        if(breakKey == true) {
+            FSet temp = FSet('\0', "");
+
+            /* Do bubble sort to sort nonterminal from small to big of ascii code */
+            for(int i = 0; i < followSets.size(); i++) {
+                for(int j = 0; j < followSets.size() - 1; j++) {
+                    if(followSets.at(j).nonTerminal > followSets.at(j + 1).nonTerminal) {
+                        temp = followSets.at(j + 1);
+                        followSets.at(j + 1) = followSets.at(j);
+                        followSets.at(j) = temp;
+                    }
+                }
+            }
+
+            /* Print the result */
+            for(int i = 0; i < followSets.size(); i++) {
+                cout << followSets.at(i).nonTerminal << " " << followSets.at(i).set << endl;
+            }
+            
+            break;
+        }
     }
 
     return 0;
 }
 
-int getFollowSetIndex(vector<FollowSet> *followSetPointer, char element) {
-    for(int i = 0; i < followSetPointer->size(); i++) {
-        if(element == followSetPointer->at(i).nonTerminal) {
+int getRuleIndex(vector<Rule> *rulePointer, char element) {
+    for(int i = 0; i < rulePointer->size(); i++) {
+        if(element == rulePointer->at(i).nonTerminal) {
             return i;
         }
     }
-
-    cout << "AAA " << element << endl;
 
     return -1;
 }
 
-int getFirstSetIndex(vector<FirstSet> *firstSetPointer, char element) {
-    for(int i = 0; i < firstSetPointer->size(); i++) {
-        if(element == firstSetPointer->at(i).nonTerminal) {
+int getFSetIndex(vector<FSet> *fSetPointer, char element) {
+    for(int i = 0; i < fSetPointer->size(); i++) {
+        if(element == fSetPointer->at(i).nonTerminal) {
             return i;
         }
     }
-
-    cout << "BBB " << element << endl;
 
     return -1;
 }
